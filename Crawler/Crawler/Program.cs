6a65a190830c7269335c22fe645a9ea7cc74759e
Rohan Khandelwal <Rohan Khandelwal>
2016-11-14 22:01:53 -0800

    initial version of crawler

---
+++ Crawler/Crawler/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abot.Crawler;
+using Abot.Poco;
+using System.Net;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+using System.Globalization;
+
+namespace Crawler
+{
+    class Program
+    {
+        private static void crawler_ProcessPageCrawlStarting(object sender, PageCrawlStartingArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            //Console.WriteLine("About to crawl link {0} which was found on page {1}", pageToCrawl.Uri.AbsoluteUri, pageToCrawl.ParentUri.AbsoluteUri);
+        }
+
+        private static void crawler_ProcessPageCrawlCompleted(object sender, PageCrawlCompletedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+
+            if (crawledPage.WebException != null || crawledPage.HttpWebResponse.StatusCode != HttpStatusCode.OK)
+                Console.WriteLine("Crawl of page failed {0}", crawledPage.Uri.AbsoluteUri);
+            else
+            { 
+                //Console.WriteLine("Crawl of page succeeded {0}", crawledPage.Uri.AbsoluteUri);
+                //Console.WriteLine(crawledPage.HtmlDocument.DocumentNode.InnerHtml);
+                var quotes = crawledPage.HtmlDocument.DocumentNode.SelectNodes("//div[@class='quoteText']");
+                foreach(var quote in quotes)
+                {
+                    var readableQuote = Regex.Match(quote.InnerHtml, @"&ldquo;(.*)&rdquo;").Groups[1].Value;
+                    Console.WriteLine(string.Format(CultureInfo.InvariantCulture,"<quote>\n{0}\n</quote>", RemoveUnwantedTags(readableQuote)));
+                }
+            }
+            if (string.IsNullOrEmpty(crawledPage.Content.Text))
+                Console.WriteLine("Page had no content {0}", crawledPage.Uri.AbsoluteUri);
+
+            var htmlAgilityPackDocument = crawledPage.HtmlDocument; //Html Agility Pack parser
+            var angleSharpHtmlDocument = crawledPage.AngleSharpHtmlDocument; //AngleSharp parser
+        }
+
+        private static void crawler_PageLinksCrawlDisallowed(object sender, PageLinksCrawlDisallowedArgs e)
+        {
+            CrawledPage crawledPage = e.CrawledPage;
+            Console.WriteLine("Did not crawl the links on page {0} due to {1}", crawledPage.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+        private static void crawler_PageCrawlDisallowed(object sender, PageCrawlDisallowedArgs e)
+        {
+            PageToCrawl pageToCrawl = e.PageToCrawl;
+            Console.WriteLine("Did not crawl page {0} due to {1}", pageToCrawl.Uri.AbsoluteUri, e.DisallowedReason);
+        }
+
+        static void Main(string[] args)
+        {
+            PoliteWebCrawler crawler = new PoliteWebCrawler();
+
+            crawler.PageCrawlStartingAsync += crawler_ProcessPageCrawlStarting;
+            crawler.PageCrawlCompletedAsync += crawler_ProcessPageCrawlCompleted;
+            crawler.PageCrawlDisallowedAsync += crawler_PageCrawlDisallowed;
+            crawler.PageLinksCrawlDisallowedAsync += crawler_PageLinksCrawlDisallowed;
+
+            CrawlResult result = crawler.Crawl(new Uri("https://www.goodreads.com/quotes/tag/motivation?page=2"));
+            //This is synchronous, it will not go to the next line until the crawl has completed
+            //if (result.ErrorOccurred)
+            //    Console.WriteLine("Crawl of {0} completed with error: {1}", result.RootUri.AbsoluteUri, result.ErrorException.Message);
+            //else
+            //    Console.WriteLine("Crawl of {0} completed without error.", result.RootUri.AbsoluteUri);
+
+            Console.ReadLine();
+
+        }
+
+        internal static string RemoveUnwantedTags(string data)
+        {
+            if (string.IsNullOrEmpty(data)) return string.Empty;
+
+            var document = new HtmlDocument();
+            document.LoadHtml(data);
+
+            var acceptableTags = new String[] { "strong", "em", "u" };
+
+            var nodes = new Queue<HtmlNode>(document.DocumentNode.SelectNodes("./*|./text()"));
+            while (nodes.Count > 0)
+            {
+                var node = nodes.Dequeue();
+                var parentNode = node.ParentNode;
+
+                if (!acceptableTags.Contains(node.Name) && node.Name != "#text")
+                {
+                    var childNodes = node.SelectNodes("./*|./text()");
+
+                    if (childNodes != null)
+                    {
+                        foreach (var child in childNodes)
+                        {
+                            nodes.Enqueue(child);
+                            parentNode.InsertBefore(child, node);
+                        }
+                    }
+
+                    parentNode.RemoveChild(node);
+
+                }
+            }
+
+            return document.DocumentNode.InnerHtml;
+        }
+    }
+}
